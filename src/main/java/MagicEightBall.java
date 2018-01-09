f0a4def166b03be422b336d22728489fb3016400
Robert Henderson <robert@nterpryz.com>
2018-01-09 08:48:11 +0000

    added more to runner

--- src/main/java/MagicEightBall.java
+++ src/main/java/MagicEightBall.java
@@ -2,7 +2,7 @@ public class MagicEightBall {
 
     private String answer;
 
-    public Bear(String answer) {
+    public MagicEightBall(String answer) {
         this.answer = answer;
     }
 }
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,3 +1,5 @@
 public class Runner {
     public static void main(String[] args) {
         MagicEightBall 1 = new MagicEightBall("Yes");
+        MagicEightBall 2 = new MagicEightBall("No");
+        MagicEightBall 3 = new MagicEightBall("Maybe");
